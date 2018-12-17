a455778bfff48ef91010ae8a34da1df07f463fae
Arti Zirk <arti.zirk@gmail.com>
2018-12-17 13:22:35 +0200

    Add prebuild check for required host tools
    Signed-off-by: Arti Zirk <arti.zirk@gmail.com>

--- minimalbuild/minimalbuild.sh
+++ minimalbuild/minimalbuild.sh
@@ -8,6 +8,7 @@
 # - tree
 
 TARGET=minimalbuild
+TOOLS=('sudo' 'pacstrap' 'du' 'tree' 'fakeroot' 'mksquashfs' 'diff')
 
 rm -rf minimalbuild && mkdir -p minimalbuild
 rm -rf minimalbuild.squashfs
@@ -18,6 +19,13 @@ if [ -f ./minimalbuild_report.txt ]; then
     fi
     mv minimalbuild_report.txt minimalbuild_report.txt.old
 fi
+
+for t in "${TOOLS[@]}"; do
+    if ! command -v "${t}" 1>/dev/null; then
+        echo "Error: Tool '${t}' not found in path. Check the requirements";
+        exit 1
+    fi 
+done
 
 # ffs pacstrap
 sudo pacstrap -C ./minimalbuild_pacman.conf -M -G "${TARGET}" base-minimal
